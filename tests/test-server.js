d21b9b8ae5d4881e68fe89ea2f023b2a7994d35e
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-07-30 10:58:03 -0600

    updating exercise tests

--- tests/test-server.js
+++ tests/test-server.js
@@ -12,7 +12,7 @@ process.env.NODE_EN = 'test';
 describe('workouts-api', function () {
 	it('should add a single workout to the database using /api/workouts/add_workout POST', function (done) {
 		let workout_name = faker.random.word();
-		let author_name = faker.fake('{{name.firstName}}, {{name.lastName}}');
+		let author_name = faker.fake('{{name.firstName}} {{name.lastName}}');
 		chai.request(server)
 			.post('/api/workouts/add_workout')
 			.send({
@@ -24,15 +24,51 @@ describe('workouts-api', function () {
 				res.should.be.json;
 				res.body.should.be.a('object');
 				res.body.should.have.property('workout');
+				res.body.workout.should.have.property('_id');
 				res.body.workout.should.have.property('workout_name');
 				res.body.workout.should.have.property('author');
-				//res.body.workout_name.should.have.property()
+				res.body.workout.workout_name.should.equal(workout_name);
+				res.body.workout.author.should.equal(author_name);
 				done();
 			})
 	});
 	it('should return all workouts in the database using /api/workouts GET', function (done) {
 		chai.request(server)
 			.get('/api/workouts')
+			.end(function (err, res) {
+				res.should.have.status(200);
+				done();
+			});
+	});
+});
+
+describe('exercises-api', function () {
+	it('should add a single exercise to the database using /api/exercises/add_exercise POST', function (done) {
+		let exercise_name = faker.random.word();
+		let author_name = faker.fake('{{name.firstName}} {{name.lastName}}');
+		chai.request(server)
+			.post('/api/exercises/add_exercise')
+			.send({
+				exercise_name: exercise_name,
+				author: author_name,
+			})
+			.end(function (err, res) {
+				console.log(res);
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.have.property('exercise');
+				res.body.workout.should.have.property('_id');
+				res.body.workout.should.have.property('exercise_name');
+				res.body.workout.should.have.property('author');
+				res.body.workout.workout_name.should.equal(exercise_name);
+				res.body.workout.author.should.equal(author_name);
+				done();
+			})
+	});
+	it('should return all workouts in the database using /api/exercises GET', function (done) {
+		chai.request(server)
+			.get('/api/exercises')
 			.end(function (err, res) {
 				res.should.have.status(200);
 				done();
